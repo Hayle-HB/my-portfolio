eb10cd751c77ca86098c4a288f9abd380b899630
= <=>
2025-05-06 17:26:47 -0300

    m

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
     },
     {
       name: "LinkedIn",
-      url: "https://linkedin.com/in/Hayle_HB",
+      url: "https://www.linkedin.com/in/haylemeskel-haylemariam-b9212b298/",
       icon: (
         <svg
           className="w-6 h-6"
@@ -125,7 +125,7 @@ const Footer = () => {
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
-                <span>contact@haylemeskel.com</span>
+                <span>haylemeskelhaylemariam@gmail.com</span>
               </li>
               <li className="flex items-center space-x-3">
                 <svg
